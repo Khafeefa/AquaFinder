e15ff3e9d86dd66b8c0208af3c3fa38ddc32cd03
Khafeefa <kelmalaki11@gmail.com>
2025-11-03 21:45:38 -0700

    Create AquaFindApp.swift

---
+++ AquaFindApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaFindApp.swift
+//  AquaFinder
+//
+//  Main entry point for the AquaFinder iOS application
+//
+
+import SwiftUI
+
+@main
+struct AquaFindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
